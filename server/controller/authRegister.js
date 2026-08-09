5d340839fff1e620379bb07a4df6a7144ba47777
Nwigwe Chidera <ch1h23.y9@gmail.com>
2026-08-09 13:05:52 +0100

    Fix error

--- server/controller/authRegister.js
+++ server/controller/authRegister.js
@@ -114,8 +114,8 @@ export const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      secure: false,
-      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     });
 
     res.status(200).json({
