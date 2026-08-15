00f1a77c8f2510802598180280039af6f3e6538d
Nwigwe Chidera <ch1h23.y9@gmail.com>
2026-08-15 18:53:25 +0100

    fix: update icon references in Vite config to include new PNG icons

--- client/vite.config.js
+++ client/vite.config.js
@@ -27,9 +27,14 @@ export default defineConfig({
 
         icons: [
           {
-            src: "/icons.svg",
-            sizes: "any",
-            type: "image/svg+xml",
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
         ],
       },
